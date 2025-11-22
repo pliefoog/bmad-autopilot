6c2af6012694224a98ffc108c2b6c10e7060511e
pliefoog <pieter.liefooghe@gmail.com>
2025-11-22 16:46:07 +0100

    fix: remove web-polyfills import from index.js
    Removed import of web-polyfills.js from index.js
    This polyfill was moved to old/ directory and is no longer needed.

--- boatingInstrumentsApp/index.js
+++ boatingInstrumentsApp/index.js
@@ -3,7 +3,4 @@
  * Entry point for Expo Router file-based navigation
  */
 
-// Load web polyfills first (for Node.js globals and Expo env vars)
-import './web-polyfills';
-
 import 'expo-router/entry';
