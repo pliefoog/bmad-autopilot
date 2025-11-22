12266fff85e681950b41a234346b5a71b167320e
pliefoog <pieter.liefooghe@gmail.com>
2025-11-22 21:36:21 +0100

    Use theme.interactive for Switch track colors in AlarmConfigDialog for consistency

--- boatingInstrumentsApp/src/components/dialogs/AlarmConfigDialog.tsx
+++ boatingInstrumentsApp/src/components/dialogs/AlarmConfigDialog.tsx
@@ -377,7 +377,6 @@ export const AlarmConfigDialog: React.FC<AlarmConfigDialogProps> = ({
                       value={isEnabled}
                       onValueChange={(value) => handleQuickToggle(alarm.type, value)}
                       trackColor={{ false: theme.border, true: theme.interactive }}
-                      ios_backgroundColor={theme.border}
                       disabled={alarm.type === CriticalAlarmType.SHALLOW_WATER && !isEnabled}
                     />
                   </View>
@@ -432,7 +431,6 @@ export const AlarmConfigDialog: React.FC<AlarmConfigDialogProps> = ({
               value={config.enabled}
               onValueChange={(value) => updateConfig({ enabled: value })}
               trackColor={{ false: theme.border, true: theme.interactive }}
-              ios_backgroundColor={theme.border}
               disabled={selectedAlarmType === CriticalAlarmType.SHALLOW_WATER && config.enabled}
             />
           </View>
