66ef0ae474212d79fe2b9333de889e6c79dbec5f
pliefoog <pieter.liefooghe@gmail.com>
2026-01-09 21:04:13 +0100

    Refactor NavigationWidget: Remove subscriptions, pure layout pattern

--- boatingInstrumentsApp/src/widgets/NavigationWidget.tsx
+++ boatingInstrumentsApp/src/widgets/NavigationWidget.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useNmeaStore } from '../store/nmeaStore';
 import TemplatedWidget from '../components/TemplatedWidget';
 import PrimaryMetricCell from '../components/PrimaryMetricCell';
-import SecondaryMetricCell from '../components/SecondaryMetricCell';
 
 interface NavigationWidgetProps {
   id: string;
@@ -16,18 +14,15 @@ interface NavigationWidgetProps {
  *
  * Note: Waypoint name and ETA not displayed in this compact view.
  * Consider adding 2Rx2C-SEP-2Rx1C template variant for waypoint info.
+ *
+ * NO SUBSCRIPTIONS: Widget is pure layout, TemplatedWidget handles store access
  */
 export const NavigationWidget: React.FC<NavigationWidgetProps> = React.memo(({ id }) => {
-  const navigationInstance = useNmeaStore((state) => state.nmeaData.sensors.navigation?.[0]);
-
-  // Subscribe to timestamp to trigger re-renders (SensorInstance is mutable)
-  const _timestamp = useNmeaStore((state) => state.nmeaData.sensors.navigation?.[0]?.timestamp);
-
   return (
     <TemplatedWidget
       template="4Rx2C-NONE"
-      sensorInstance={navigationInstance}
       sensorType="navigation"
+      instanceNumber={0}
       testID={id}
     >
       {/* Primary Grid: Navigation metrics in 2x2 layout */}
